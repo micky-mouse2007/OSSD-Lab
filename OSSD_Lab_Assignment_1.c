9fba27d4e9342ff69ebb009615f420a177845880
micky-mouse2007 <46025627+micky-mouse2007@users.noreply.github.com>
2021-06-03 14:32:27 +0530

    Add files via upload

---
+++ OSSD_Lab_Assignment_1.c
@@ -0,0 +1,46 @@
+
+# include <stdio.h>
+# include <string.h>
+
+int main( )
+{
+
+    // Declare the file pointer
+    FILE *filePointer ;
+
+    // Get the data to be written in file
+    char dataToBeWritten[50]
+        = "OSSD Lab";
+
+    // Open the existing file GfgTest.c using fopen()
+    // in write mode using "w" attribute
+    filePointer = fopen("OSSD.c", "w") ;
+
+    // Check if this filePointer is null
+    // which maybe if the file does not exist
+    if ( filePointer == NULL )
+    {
+        printf( "OSSD.c file failed to open." ) ;
+    }
+    else
+    {
+
+        printf("The file is now opened.\n") ;
+
+        // Write the dataToBeWritten into the file
+        if ( strlen (  dataToBeWritten  ) > 0 )
+        {
+
+            // writing in the file using fputs()
+            fputs(dataToBeWritten, filePointer) ;
+            fputs("\n", filePointer) ;
+        }
+
+        // Closing the file using fclose()
+        fclose(filePointer) ;
+
+        printf("Data successfully written in file OSSD.c\n");
+        printf("The file is now closed.") ;
+    }
+    return 0;
+}
